25951895455fd921a1ccc7fb56aef052c715ad1e
Diyan Boshnakov <111239673+dboshnakov@users.noreply.github.com>
2023-07-02 02:43:42 +0300

    Fix options section Add eraser & show grid functions
    To add:
    - various options:
      - color picker
      - erase current grid
    
    To fix:
    - after grid resize, show grid stops working

--- etch.js
+++ etch.js
@@ -1,5 +1,6 @@
 let body = document.querySelector("body");
 let number = 16;
+let gridMode = false;
 
 let sectionMain = document.createElement("div");
 let sectionOptions = document.createElement("div");
@@ -8,25 +9,54 @@ let sectionSlider = document.createElement("div");
 let options = document.createElement("div");
 let sliderText = document.createElement("div");
 let sizeSlider = document.createElement("input");
+let erase = document.createElement("div");
+let showGrid = document.createElement("div");
+let flagEraser = document.createElement("input");
+let flagShowGrid = document.createElement("input");
 
 sectionMain.classList.add("section-main");
 sectionOptions.classList.add("section-options");
 sectionGrid.classList.add("section-grid");
 sectionSlider.classList.add("section-slider");
 options.classList.add("options");
+erase.classList.add("option");
+erase.classList.add("erase");
+showGrid.classList.add("option");
+showGrid.classList.add("show-grid");
+flagEraser.classList.add("checkbox");
+flagShowGrid.classList.add("checkbox");
+labelEraser = document.createElement("label");
+labelShowGrid = document.createElement("label");
 
+erase.appendChild(flagEraser);
+erase.appendChild(labelEraser);
+showGrid.appendChild(flagShowGrid);
+showGrid.appendChild(labelShowGrid);
+options.appendChild(erase);
+options.appendChild(showGrid);
 sectionSlider.appendChild(sliderText);
 sectionSlider.appendChild(sizeSlider);
-sectionOptions.appendChild(options);
 sectionOptions.appendChild(sectionSlider);
+sectionOptions.appendChild(options);
 sectionMain.appendChild(sectionOptions);
 sectionMain.appendChild(sectionGrid);
 body.appendChild(sectionMain);
 
-options.textContent = `lorem ipsum random text lorem ipsum random textlorem ipsum random textlorem ipsum random textlorem ipsum random text`;
+flagEraser.setAttribute("id","eraser");
+labelEraser.setAttribute("for","eraser");
+labelEraser.textContent ="Eraser";
+flagShowGrid.setAttribute("id","display-grid");
+labelShowGrid.setAttribute("for","display-grid");
+labelShowGrid.textContent ="Show grid";
 
 sliderText.classList.add("slider-text");
 sliderText.textContent = `Grid size: ${number}x${number}`;
+
+flagEraser.setAttribute("type","checkbox");
+flagEraser.setAttribute("name","eraser");
+
+flagShowGrid.setAttribute("type","checkbox");
+flagShowGrid.setAttribute("name","color-picker");
 
 sizeSlider.textContent = "Grid size";
 sizeSlider.setAttribute("type","range");
@@ -42,21 +72,34 @@ sizeSlider.addEventListener("mouseup",function() {
     setGrid(number);
 });
 
-
 sizeSlider.oninput = function() {
     number = this.value;
     sliderText.textContent = `Grid size: ${number}x${number}`;
 }
 
+flagShowGrid.addEventListener("change",function() {
+    if (this.checked) {
+        gridMode = true;
+    } else {
+        gridMode = false;
+    }
+    console.log(gridMode);
+    displayGrid();
+});
 
-
+function displayGrid() {
+    console.log(cells);
+    if (gridMode) {
+        cells.forEach(cell => cell.classList.add("with-border"));
+    } else {
+        cells.forEach(cell => cell.classList.remove("with-border"));
+    }
+}
 
 
 function setGrid(number) {
     removeExistingGrid();
-    //console.log(`number = ${number}`);
     for (let i = 1; i <= number; i++) {
-        //console.log(`i=${i}, number=${number}`);
         let row = document.createElement("div");
         row.classList.add("row");
         row.classList.add(i);
@@ -73,6 +116,7 @@ function setGrid(number) {
     }
 }
 
+
 let grid = document.createElement("div");
 grid.classList.add("grid");
 setGrid(number);
@@ -81,83 +125,63 @@ function resizeGrid() {
     const userInput = prompt("Grid size: X by X. Enter X (1 to 100)");
     if (userInput === null || userInput === "") {
     } else if (userInput > 100 || userInput < 1) {
-        //console.log(userInput);
         alert("Invalid input. Please enter a number value between 1 and 100");
         resizeGrid();
     } else {
         console.log(`Grid size ${userInput} by ${userInput}`);
         number = userInput;
-        //console.log(number);
         sliderText.textContent = number;
         setGrid(number);
     }
 }
 
-
 function removeExistingGrid() {
     if (grid.firstChild === null) {
-        //console.log("no child elements");
     }
-    //console.log(grid.firstChild);
     while (grid.firstChild !== null) {
-        //console.log(grid.firstChild);
         grid.removeChild(grid.firstChild);
     }
 }
 
-
-
 let viewport = document.querySelector("html");
 let isClicked = false;
+let eraserMode = false;
+
+
+let cells = document.querySelectorAll(".item");
+
+
+
+
 
 viewport.addEventListener("mousedown",holdClick,false);
 viewport.addEventListener("mouseup",unholdClick,false);
 viewport.addEventListener("mouseover",markCell,false);
+flagEraser.addEventListener("change",function() {
+    if (this.checked) {
+        eraserMode = true;
+    } else {
+        eraserMode = false;
+    }
+});
 
 function holdClick(e) {
     isClicked = true;
-    //console.log(isClicked,e.target);
     markCell(e);
 }
 
 function unholdClick() {
     isClicked = false;
-    //console.log(isClicked);
 }
 
 function markCell(e) {
-    //console.log(isClicked,e.target);
     if (isClicked && e.target.classList.contains('item')) {
-        e.target.classList.add("marked");
+        if (!eraserMode) {
+            e.target.classList.add("marked");
+        } else {
+            e.target.classList.remove("marked");
+        }
     } else if (!e.target.classList.contains('item')) {
         isClicked=false;
     }
 }
-
-
-
-
-//table.addEventListener("mouseover", clickHeld, false);
-//table.addEventListener("click", markCell, false);
-//table.addEventListener("mousedown",clickHeld);
-//table.addEventListener("mouseup",clickReleased);
-
-/*function clickHeld(e) {
-    isClicked = true;
-    console.log("click held");
-    }
-
-function clickReleased() {
-    isClicked = false;
-    console.log("click released");
-}
-
-function markCell(e) {
-    table.addEventListener("mousedown",clickHeld);
-    table.addEventListener("mouseup",clickReleased);
-    //console.log(isClicked);
-    if (e.target.classList.contains('item')) {
-        e.target.classList.add("marked");
-    }
-    
-}*/
